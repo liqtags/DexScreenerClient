f1b6c9239fb6d8e61b185a9fe778192d9482123e
0xlino <13621351+0xlino@users.noreply.github.com>
2023-12-30 16:34:45 +0000

    Add function to add data to Supabase table

---
+++ add_data_to_supabase.py
@@ -0,0 +1,22 @@
+from consts import SUPABASE_URL, SUPABASE_KEY
+from supabase import create_client, Client
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def add_data_to_supabase(pairsType, pair, createscanLink):
+    """
+    Add data to Supabase table.
+
+    Args:
+        pairsType (str): The type of pairs.
+        pair (dict): The pair data.
+        createscanLink (str): The scan link.
+
+    Returns:
+        None
+    """
+    supabase.table(pairsType).insert({
+                "name": pair.get("baseToken").get("name"),
+                "scan": createscanLink,
+                "json_data": pair, 
+                "type": pairsType
+    }).execute()
